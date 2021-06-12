52c7c84b672049d9e070521b250bcdbe6f4065ad
AlexanderParshakov <alls.parshakov@gmail.com>
2021-06-12 23:59:22 +0300

    Initial Commit

---
+++ DictionaryAPI/DictionaryAPI.h
@@ -0,0 +1,18 @@
+//
+//  DictionaryAPI.h
+//  DictionaryAPI
+//
+//  Created by Parshakov Alexander on 6/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DictionaryAPI.
+FOUNDATION_EXPORT double DictionaryAPIVersionNumber;
+
+//! Project version string for DictionaryAPI.
+FOUNDATION_EXPORT const unsigned char DictionaryAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DictionaryAPI/PublicHeader.h>
+
+
